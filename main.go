6bedfeeebff4228cab51b2034a7818af710d8be5
Nato Boram <nato.boram@gmail.com>
2018-03-24 17:05:56 -0400

    Can start the spider

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"database/sql"
 
 	"github.com/NatoBoram/Purraka/config"
+	"github.com/NatoBoram/Purraka/spider"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var db *sql.DB
 
 func main() {
 
@@ -13,18 +16,31 @@ func main() {
 	err := config.Load()
 	if err != nil {
 		println("Couldn't load the config file.")
-		println(err.Error)
-		config.Reset()
+		println(err.Error())
+		// config.Reset()
+		return
 	}
 
 	// Open a connection to the database.
-	db, err := sql.Open("mysql",
-		config.Config.user+":"+Config.password+"@tcp("+Config.address+":"+Config.port+")/"+Config.database)
+	db, err = sql.Open("mysql", config.DBConfig.User+":"+config.DBConfig.Password+"@tcp("+config.DBConfig.Address+":"+config.DBConfig.Port+")/"+config.DBConfig.Database)
 	if err != nil {
-		println(err.Error)
+		println("Couldn't connect to the database.")
+		println(err.Error())
+		return
 	}
-
-	//
-
 	defer db.Close()
+
+	// Check version
+	var version string
+	db.QueryRow("SELECT VERSION()").Scan(&version)
+	println("Connected to :", version)
+
+	// Open Discord Bot
+
+	// Start the spider
+	spider.Spider()
+	go spider.StartSpider()
+
+	// Wait for infinity
+	<-make(chan struct{})
 }
